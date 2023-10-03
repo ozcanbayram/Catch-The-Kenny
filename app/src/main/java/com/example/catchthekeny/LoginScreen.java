e504a62e666ede3b99bb693eeb8b17c5676cc666
ozcanbayram <ozzcanbayram@gmail.com>
2023-10-03 16:23:55 +0300

    Start button complated.

--- app/src/main/java/com/example/catchthekeny/LoginScreen.java
+++ app/src/main/java/com/example/catchthekeny/LoginScreen.java
@@ -2,7 +2,9 @@ package com.example.catchthekeny;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginScreen extends AppCompatActivity {
 
@@ -11,4 +13,11 @@ public class LoginScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
     }
+
+    public void start(View view){
+        Intent intent = new Intent(LoginScreen.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
 }
